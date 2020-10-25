dc043f39e5e54fe8d6666487d37ce68231e61ca5
Rohan Panwar <rohan.panwar@neurontechlabs.com>
2020-10-25 21:49:52 +0530

    styled name

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Content({data, searchTerm}) {
   console.log("CONTENTS", data)
     return (
-        <div className="mx-3 mb-10">
+        <div className="mx-3 mb-10 w-1/4">
             <img src={require(`../slices/${data["poster-image"]}`)} loading="lazy" className="w-24 md:w-11/12" alt="content_image" placeholder="Loading..."/>
             <h1 className="text-white text-xs md:text-sm">{data.name}</h1>
         </div>
