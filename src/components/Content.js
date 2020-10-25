20cee43175fedb14ed1a0900fddeb28072130e84
Rohan Panwar <rohan.panwar@neurontechlabs.com>
2020-10-25 20:19:07 +0530

    final commit added loading lazy attribute

--- src/components/Content.js
+++ src/components/Content.js
@@ -10,10 +10,10 @@ export default function Content({data, searchTerm}) {
   console.log("CONTENTS", data)
     return (
         <div className="mx-3 mb-10">
-          <LazyLoad placeholder={<Loading />}>
-            <img src={require(`../slices/${data["poster-image"]}`)} className="w-24 md:w-11/12" alt="content_image"/>
+          {/* <LazyLoad placeholder={<Loading />}> */}
+            <img src={require(`../slices/${data["poster-image"]}`)} loading="lazy" className="w-24 md:w-11/12" alt="content_image" placeholder="Loading..."/>
             <h1 className="text-white text-xs md:text-sm">{data.name}</h1>
-          </LazyLoad>
+          {/* </LazyLoad> */}
         </div>
     )
 }
